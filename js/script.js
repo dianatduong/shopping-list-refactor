5204d79d7a31ad481354bf847c414ce10d46c79d
Diana Duong <dduong013@gmail.com>
2022-06-30 16:00:21 -0500

    rename li variable

--- js/script.js
+++ js/script.js
@@ -6,19 +6,19 @@ btn.addEventListener('click', () => {
   let newItem = input.value;
   input.value = '';
 
-  let item = document.createElement('li');
+  let listItem = document.createElement('li');
   //let itemText = document.createElement('span');
   let deleteBtn = document.createElement('button');
 
-  item.textContent = newItem;
+  listItem.textContent = newItem;
   deleteBtn.textContent = 'Delete';
 
   if (!newItem) {
     alert("Please enter an item.")
   } else {
-    shoppingList.prepend(item);
-    item.appendChild(deleteBtn);
-    item.classList.add('list-item');
+    shoppingList.prepend(listItem);
+    listItem.appendChild(deleteBtn);
+    listItem.classList.add('list-item');
     deleteBtn.classList.add('delete');
   }
 
